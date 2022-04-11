025087a328acee60420f29f4e58ccbb08525c4dc
danielml1003 <danielbaravik1003@gmail.com>
2022-04-11 21:30:58 +0300

    made a tsx project deleted some unnecessary files and then did a hello world so i can start

---
+++ kawaii-client/src/index.tsx
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>NIGUS</h1>
+ReactDOM.render(element, document.getElementById("root"));
