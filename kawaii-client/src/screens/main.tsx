209c56f4897a85c8e8f6da8f9fba822b9a72325a
danielml1003 <danielbaravik1003@gmail.com>
2022-04-13 20:18:47 +0300

    trying to make connection

--- kawaii-client/src/screens/main.tsx
+++ kawaii-client/src/screens/main.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { io } from "socket.io-client";
+import { ReactMediaRecorder } from "react-media-recorder";
+
 
 export function Main() {
-    return <h1>Hello world</h1>
+    const socket = io("http://localhost:3000");
+    //const connectToServer = () => {socket.on("connect", () => console.log(socket.id))}
+    socket.on("connect", () => console.log(socket.id))
+
+    return (
+        <h1>Hello world </h1>
+    );
 }
+
+
+
