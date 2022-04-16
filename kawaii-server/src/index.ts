5c430c6841771341fa412e41758b83e77f6cf8f7
FreeMotion45 <thewebsiter@gmail.com>
2022-04-17 00:59:14 +0300

    Fixed cors + fixed module imports for TS

--- kawaii-server/src/index.ts
+++ kawaii-server/src/index.ts
@@ -1,7 +1,21 @@
 import { Server } from 'socket.io'
-import { KawaiiServer } from './kawaiiServer.js'
+import { KawaiiServer } from './kawaiiServer'
+import express from 'express'
+import { createServer as createHttpServer } from 'http'
+
+const expressHost = express()
+
+const httpServer = createHttpServer(expressHost)
+
 
 const PORT = 42069
 
-const io = new Server(PORT)
+const httpOptions = {
+    cors: {
+        origin: '*'
+    }
+}
+const io = new Server(httpServer, httpOptions)
 const kws = new KawaiiServer(io)
+
+httpServer.listen(PORT)
