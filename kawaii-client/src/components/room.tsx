97a418a42d3cc49ce0e9ce693f857f5b04f3b67f
danielml1003 <danielbaravik1003@gmail.com>
2022-04-16 22:19:08 +0300

    made a room component

---
+++ kawaii-client/src/components/room.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export function room (){
+
+    return (
+        <h1>...</h1>
+    );
+}
--- kawaii-client/src/screens/main.tsx
+++ kawaii-client/src/screens/main.tsx
@@ -4,12 +4,14 @@ import { ReactMediaRecorder } from "react-media-recorder";
 
 
 export function Main() {
-    const socket = io("http://localhost:3000");
+    const socket = io("http://localhost:42069");
     //const connectToServer = () => {socket.on("connect", () => console.log(socket.id))}
     socket.on("connect", () => console.log(socket.id))
 
+
+    
     return (
-        <h1>Hello world </h1>
+        <h1>Hello world</h1>
     );
 }
 
