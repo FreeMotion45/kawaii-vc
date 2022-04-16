e5bad1242dc40aec7b82441bed7bb05c57af48dd
FreeMotion45 <thewebsiter@gmail.com>
2022-04-17 02:01:52 +0300

    Started working on a simple voice chat room

--- kawaii-server/src/vc/voiceChatRoom.ts
+++ kawaii-server/src/vc/voiceChatRoom.ts
@@ -1,6 +1,33 @@
+import { Socket } from "socket.io";
+
 class VoiceChatRoom {
-    users: any[];
+    private userIOs: Socket[]
+    private audioBuffers: Map<string, any[]>
     
-    constructor() {        
+    constructor() { }
+
+    public addUser(userIO: Socket) {
+        if (this.userIOs.includes(userIO)) {
+            throw new Error(`User ${userIO.id} already exists in the current VC room!`);
+        }
+
+        this.userIOs.push(userIO)
+    }
+
+    public removeUser(userIO: Socket) {
+        const userIOIndex = this.userIOs.indexOf(userIO)
+        this.userIOs.splice(userIOIndex, 1)
+    }
+
+    public resetAudioSlice() {
+        this.audioBuffers.clear()
+    }
+
+    public addAudioBuffer(userIO: Socket, audioBuffer: any) {
+        if (!this.audioBuffers.has(userIO.id)) {
+            this.audioBuffers[userIO.id] = []
+        }
+
+        this.audioBuffers[userIO.id].push(audioBuffer)
     }
 }
