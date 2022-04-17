11f9a4398be638e663e6c7b378bc3d8a56d9b1fc
FreeMotion45 <thewebsiter@gmail.com>
2022-04-17 16:17:56 +0300

    trying to fix cors

--- kawaii-server/src/index.ts
+++ kawaii-server/src/index.ts
@@ -5,13 +5,12 @@ import * as express from 'express'
 import * as cors from 'cors'
 
 const app = express()
-app.use(cors())
 
 const httpServer = createHttpServer(app)
 
 const io = new Server(httpServer, {
     cors: {
-        origin: '*'
+        origin: "*"
     }
 })
 registerKawaiiServerSocketIO(io)
