913827f434ea9172f2bbbbae41cb2edef75a331e
FreeMotion45 <thewebsiter@gmail.com>
2022-04-20 01:59:46 +0300

    Made events to join and exit voice channels

--- kawaii-server/src/kawaiiServer.ts
+++ kawaii-server/src/kawaiiServer.ts
@@ -1,26 +1,61 @@
 import { Server, Socket } from "socket.io";
+import { VoiceChannel } from "./vc/voiceChannel";
 
 class KawaiiServer {
-    private io: Server    
+    private io: Server
+    private voiceChannels: VoiceChannel[]
 
     constructor(io: Server) {
-        this.io = io
-        console.log('Dani starts sucking the D!')
-
+        this.io = io        
         this.initialize()
+    }
+
+    private initialize() {
+        console.log('Registering Dani SUCC events...')
+        
+        this.io.on('connect', this.onUserConnect)
+        console.log('Registered To Dani connect event!')
+
+        this.io.on('disconnect', this.onUserDisconnect)        
+        console.log('Registered To Dani dickonnect event!')
     }
 
     private onUserConnect(userIo: Socket) {
         console.log(`${userIo.id} connected!`)
+
+        userIo.on('join voice channel', (voiceChannelName) => this.onJoinVoiceChannel(userIo, voiceChannelName))
+        userIo.on('exit voice channel', () => this.onExitVoiceChannel(userIo))
     }
 
     private onUserDisconnect(userIo: Socket) {
         console.log(`${userIo.id} disconnected!`)
     }
 
-    private initialize() {
-        this.io.on('connect', this.onUserConnect)
-        this.io.on('disconnect', this.onUserDisconnect)
+    private onJoinVoiceChannel(userIO: Socket, voiceChannelName: string) {
+        const voiceChannel = this.getVoiceChannelByName(voiceChannelName)
+        voiceChannel.addUser(userIO)
+
+        userIO.on('voice', (voiceBuffer) => voiceChannel.streamUserVoice(userIO, voiceBuffer))
+    }
+
+    private onExitVoiceChannel(userIO: Socket) {
+        for (const voiceChannel of this.voiceChannels) {
+            if (voiceChannel.userExists(userIO)) {
+                voiceChannel.removeUser(userIO)
+                
+                userIO.removeAllListeners('voice')
+            }
+        }
+    }
+
+    private getVoiceChannelByName(voiceChannelName: string) : VoiceChannel {
+        for (const voiceChannel of this.voiceChannels) {
+            if (voiceChannel.name == voiceChannelName) {
+                return voiceChannel
+            }
+        }
+
+        return undefined
     }
 }
 
---
+++ kawaii-server/src/vc/voiceChannel.ts
@@ -0,0 +1,35 @@
+import { Socket } from "socket.io";
+
+export class VoiceChannel {
+    public name: string
+
+    private userIOs: Socket[]   
+    
+    constructor(name: string) {
+        this.name = name
+    }
+
+    public addUser(userIO: Socket) {
+        if (this.userIOs.includes(userIO)) {
+            throw new Error(`User ${userIO.id} already exists in the current VC room!`);
+        }
+
+        this.userIOs.push(userIO)
+        console.log(`${userIO.id} joined VC ${this.name}`)
+    }
+
+    public removeUser(userIO: Socket) {
+        const userIOIndex = this.userIOs.indexOf(userIO)
+        this.userIOs.splice(userIOIndex, 1)
+        console.log(`${userIO.id} exited from VC ${this.name}`)
+    }
+
+    public userExists(userIO: Socket) : boolean {
+        return this.userIOs.includes(userIO)
+    }
+
+    public streamUserVoice(userIO: Socket, audioBuffer: any) {
+        // TODO: Implement this...
+        console.log(`${userIO.id} streamd a new voice event!`)
+    }
+}
