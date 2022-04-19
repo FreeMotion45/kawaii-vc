8b6ba807dcfb817e0e47462397b8c5b33716b7fc
FreeMotion45 <thewebsiter@gmail.com>
2022-04-20 02:50:19 +0300

    Added more examples for dani

--- scripts/pyplayground.py
+++ scripts/pyplayground.py
@@ -4,5 +4,10 @@ import socketio
 sio = socketio.Client()
 sio.connect('http://localhost:42069')
 
-sio.emit('join voice channel', { 'voiceChannelName' : 'general' })
+sio.emit('join voice channel', 'general')
+
+sio.emit('voice', 'voice...')
+sio.emit('voice', 'voice...')
+sio.emit('voice', 'voice...')
+
 sio.emit('exit voice channel')
