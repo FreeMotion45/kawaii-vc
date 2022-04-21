22a569c9919b3a53931dc672e7a6ffa3d90755d5
FreeMotion45 <thewebsiter@gmail.com>
2022-04-21 23:29:02 +0300

    Made the useRecorder a custom hook :)

--- kawaii-client/src/components/room.tsx
+++ kawaii-client/src/components/room.tsx
@@ -3,12 +3,41 @@ import { AudioRecorder } from "../media/audioRecorder";
 import { Room, JoinButton, LeaveButton } from './styles/room.styles';
 
 
-export const VoiceRoom = (props: any) => {
-    const [id, setId] = useState("not joined")
-
-    const [cnt, setCnt] = useState(1)
+const useRecorder = () => {
     const audioRecorderRef = useRef(new AudioRecorder())
     const audioRecorder = audioRecorderRef.current
+    const handlers: ((ev: BlobEvent) => any)[] = []
+
+    useEffect(() => {
+        return () => {
+            for (const handler of handlers) {
+                audioRecorder.removeHandler(handler)
+                console.log('removed handler')
+            }
+        }
+    })
+
+    return {
+        addHandler: (handler: (ev: BlobEvent) => any) => {
+            handlers.push(handler)
+            audioRecorder.addHandler(handler)
+            console.log('added handler')
+        },
+
+        removeHandler: (handler: (ev: BlobEvent) => any) => {
+            handlers.splice(handlers.indexOf(handler))
+            audioRecorder.removeHandler(handler)
+        },
+
+        audioRecorder: audioRecorder,
+    }
+}
+
+
+export const VoiceRoom = (props: any) => {
+    const [id, setId] = useState("not joined")
+    const [cnt, setCnt] = useState(1)
+    const { addHandler, removeHandler, audioRecorder } = useRecorder()    
 
     const streamVoice = (ev: BlobEvent) => {
         console.log(`Voice ${cnt}`)
@@ -16,12 +45,8 @@ export const VoiceRoom = (props: any) => {
     }
 
     useEffect(() => {
-        audioRecorder.start(100)
-        audioRecorder.addHandler(streamVoice)
-
-        return () => {
-            audioRecorder.removeHandler(streamVoice)
-        }
+        addHandler(streamVoice)
+        return () => removeHandler(streamVoice)
     })
 
     return (
@@ -29,7 +54,7 @@ export const VoiceRoom = (props: any) => {
             <Room> 
                 <JoinButton onClick={() => audioRecorder.start(1000)}>join!</JoinButton>
                 <h1 >your id is: {id} </h1>
-                <LeaveButton>Exit voice channel</LeaveButton>
+                <LeaveButton onClick={() => audioRecorder.stop()}>Exit voice channel</LeaveButton>
             </Room>
         </div>
     );
