1cf28ba3f050984b94d3e1a91230ff950082ed90
FreeMotion45 <thewebsiter@gmail.com>
2022-04-23 15:55:42 +0300

    Update main.tsx

--- kawaii-client/src/screens/main.tsx
+++ kawaii-client/src/screens/main.tsx
@@ -6,56 +6,12 @@ import { AudioRecorder } from "../media/audioRecorder";
 
 
 export function Main() {
-    const { addHandler, removeHandler, audioRecorder } = useRecorder()
-    const [record, setRecord] = useState(false)
-    const socketRef = useRef(io("http://25.74.212.5:42069"));
+    const socketRef = useRef(io("http://localhost:42069"));
     const socket = socketRef.current
-    
-    let chunks: BlobPart[] = []    
-
-    const voiceTest = (ev: BlobEvent) => {
-        chunks.push(ev.data)        
-    }
-
-    useEffect(() => {
-        if (record) {
-            addHandler(voiceTest)
-        }
-
-        return () => {
-            if (record) {
-                removeHandler(voiceTest)
-            }
-        }
-    })
-
-    if (!record) {
-        /*
-                        <button onClick={async (e) => {                    
-                    await audioRecorder.start(1000)                    
-                    setRecord(true)
-                }}>
-                    Voice check start!
-                </button>
-        */
-        return (
-            <div>
-                <VoiceRoom socket={socket}/>     
-            </div>
-        );
-    }
 
     return (
         <div>
-            <VoiceRoom socket={socket}/>     
-            <button onClick={() => {                                           
-                audioRecorder.stop()   
-                const au = new Audio(window.URL.createObjectURL(new Blob(chunks)))
-                au.play()
-                setRecord(false)
-            }}>
-                Voice check stop!
-            </button>
+            <VoiceRoom socket={socket}/>           
         </div>
     );
 }
